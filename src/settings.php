84d2ba6e03758030e11c192ef704492fc99a1050
Ugho Stephan <stephan.ugho@gmail.com>
2016-01-18 10:25:23 +0100

    add extension

--- src/settings.php
+++ src/settings.php
@@ -19,6 +19,7 @@ return [
             'base_url' => "http://dev.ugho-stephan.fr/",
             'picture_path' => __DIR__ . '/../public/picture_uploaded/',
             'absolute_picture_path' => 'http://dev.ugho-stephan.fr/picture_uploaded/',
-        ]
+            'extensions_valids' => [ 'jpg' , 'jpeg' , 'gif' , 'png' ],
+        ],
     ],
 ];
