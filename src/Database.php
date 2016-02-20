dcce9e04d878bd22eaf0ee19d2523f215a700c6f
Ugho Stephan <stephan.ugho@gmail.com>
2016-02-20 18:03:48 +0100

    test

--- src/Database.php
+++ src/Database.php
@@ -48,7 +48,8 @@ class Database
     private function loadPdo() {
         try {
             // Instanciation de pdo
-            $this->pdo = new PDO('mysql:host='.$this->host.';dbname='.$this->database.';charset=utf8', $this->user, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            //$this->pdo = new PDO('mysql:host='.$this->host.';dbname='.$this->database.';charset=utf8', $this->user, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $this->pdo = new PDO('mysql:host='.$this->host.';dbname='.$this->database.'', $this->user, $this->password);
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         }
         catch( PDOException $Exception ) {
