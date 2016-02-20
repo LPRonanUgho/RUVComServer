7c3915d69b3c7504d6f1fb97dfcf10bb65df2f42
Ugho Stephan <stephan.ugho@gmail.com>
2016-02-20 18:49:55 +0100

    http to https

--- src/settings.php
+++ src/settings.php
@@ -16,9 +16,9 @@ return [
 
         // Settings for Controlleur
         'config' => [
-            'base_url' => "http://dev.ugho-stephan.fr/",
+            'base_url' => "https://dev.ugho-stephan.fr/",
             'picture_path' => __DIR__ . '/../public/picture_uploaded/',
-            'absolute_picture_path' => 'http://dev.ugho-stephan.fr/picture_uploaded/',
+            'absolute_picture_path' => 'https://dev.ugho-stephan.fr/picture_uploaded/',
             'extensions_valids' => [ 'jpg' , 'jpeg' , 'gif' , 'png' ],
             'secretToken' => 'Ok2CCaEaHngyNPMqPRcE5MkvzIAwpnrJc5zECIO9fAW9dnxI1zppPvRKu7pnU8tbeFrjjke5m8wDacadWWWOFgiLcr1xvdxhUVMA1WWTJkLiFFmFAAGwS',
             'googleApiKey' => 'AIzaSyCoFVlSOijLQC1HlQswRQ3-YI8EkeODTZQ',
